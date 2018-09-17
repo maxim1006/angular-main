6c07d8db65263e75ae7bf4f8b857cd09e940a53e
maxim1006 <maxim1006@list.ru>
2018-09-17 21:15:59 +0300

    update

--- src/app/modules/m-framework/components/ng-template-example/ngTemplateExampleComponent.ts
+++ src/app/modules/m-framework/components/ng-template-example/ngTemplateExampleComponent.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
 
 @Component({
     selector: "ng-template-example",
@@ -7,5 +7,19 @@ import {Component} from '@angular/core';
 
 export class NgTemplateExampleComponent {
     public constructor() {
+    }
+
+    @ViewChild("tmpl")
+    public _tmpl: TemplateRef<any>;
+
+    @ViewChild("someDiv", {read: ViewContainerRef})
+    public _someDiv: ViewContainerRef;
+
+    ngAfterContentInit() {
+        // this._tmpl встанет сразу после this._someDiv
+        this._someDiv.createEmbeddedView(this._tmpl, {
+            $implicit: "Max", // указав $implicit могу сделать любой let-что-то и это что-то будет implicit, например <ng-template #tmpl let-name> тут hame будет равно Max несмотря на то что явно его не указал
+            city: "Moscow"
+        });
     }
 }
