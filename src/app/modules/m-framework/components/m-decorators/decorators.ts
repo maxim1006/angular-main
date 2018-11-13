47d55af3a275ab165fe6c2f173b36f2244dfcc3d
maxim1006 <maxim1006@list.ru>
2018-11-13 08:07:17 +0300

    update

--- src/app/modules/m-framework/components/m-decorators/decorators.ts
+++ src/app/modules/m-framework/components/m-decorators/decorators.ts
@@ -46,4 +46,40 @@ export function logProperty1(...args): any {
     }
 }
 
+interface PropertyHandlers<T> {
+    beforeChange?: (newValue: T, oldValue: T) => boolean | void,
+    afterChange?: (newValue: T, oldValue: T) => void
+}
+
+export function PropertyHandler<T>(handlers: PropertyHandlers<T>): PropertyDecorator {
+
+    return (target: any, propertyKey: string): void => {
+
+        let shadowPropertyKey = `__PropertyHandler_${propertyKey}_value`;
+
+        let propertyDescriptor = {
+            configurable: true,
+            enumerable: false,
+            get: function () {
+                return this[shadowPropertyKey];
+            },
+            set: function (newValue: T) {
+                let oldValue = this[shadowPropertyKey];
+                if (newValue === oldValue) {
+                    return;
+                }
+                if (handlers.beforeChange && handlers.beforeChange.call(this, newValue, oldValue) === false) {
+                    return;
+                }
+                this[shadowPropertyKey] = newValue;
+                if (handlers.afterChange) {
+                    handlers.afterChange.call(this, newValue, oldValue);
+                }
+            }
+        };
+
+        Object.defineProperty(target, propertyKey, propertyDescriptor);
+    };
+}
+
 
--- src/app/modules/m-framework/components/m-decorators/m-decorators.component.ts
+++ src/app/modules/m-framework/components/m-decorators/m-decorators.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {decorator, decorator1, logProperty, logProperty1} from "./decorators";
+import {decorator, decorator1, logProperty, logProperty1, PropertyHandler} from "./decorators";
 
 @Component({
     selector: 'm-decorators',
@@ -7,6 +7,10 @@ import {decorator, decorator1, logProperty, logProperty1} from "./decorators";
 })
 
 export class MDecoratorsComponent implements OnInit {
+    @PropertyHandler({
+        beforeChange,
+        afterChange
+    })
     @logProperty1()
     @logProperty
     @Input()
@@ -24,44 +28,18 @@ export class MDecoratorsComponent implements OnInit {
         // this.method();
         this.a = 2;
 
+        setTimeout(() => {
+            this.a = 4;
+        });
     }
 }
 
-
-interface PropertyHandlers<T> {
-    beforeChange?: { (newValue: T, oldValue: T): boolean | void },
-    afterChange?: { (newValue: T, oldValue: T): void }
+function beforeChange (newValue, oldValue) {
+    console.log("beforeChange newValue ", newValue);
+    console.log("beforeChange oldValue ", oldValue);
 }
 
-
-
-export function PropertyHandler<T>(handlers: PropertyHandlers<T>): PropertyDecorator {
-
-    return (target: any, propertyKey: string): void => {
-
-        let spoofPropertyKey = `__PropertyHandler_${propertyKey}_value`;
-
-        let propertyDescriptor = {
-            configurable: true,
-            enumerable: false,
-            get: function () {
-                return this[spoofPropertyKey];
-            },
-            set: function (newValue: T) {
-                let oldValue = this[spoofPropertyKey];
-                if (newValue === oldValue) {
-                    return;
-                }
-                if (handlers.beforeChange && handlers.beforeChange.call(this, newValue, oldValue) === false) {
-                    return;
-                }
-                this[spoofPropertyKey] = newValue;
-                if (handlers.afterChange) {
-                    handlers.afterChange.call(this, newValue, oldValue);
-                }
-            }
-        };
-
-        Object.defineProperty(target, propertyKey, propertyDescriptor);
-    };
+function afterChange (newValue, oldValue) {
+    console.log("afterChange newValue ", newValue);
+    console.log("afterChange oldValue ", oldValue);
 }
