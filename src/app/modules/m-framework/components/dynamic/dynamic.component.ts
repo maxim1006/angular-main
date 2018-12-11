fbeab5485afd88004b3ff2dbef571b1898a451f7
maxim1006 <maxim1006@list.ru>
2018-12-11 10:02:03 +0300

    update

--- src/app/modules/m-framework/components/dynamic/dynamic.component.ts
+++ src/app/modules/m-framework/components/dynamic/dynamic.component.ts
@@ -1,5 +1,12 @@
 import {
-    Component, ComponentFactoryResolver, ComponentRef, ElementRef, EmbeddedViewRef, ViewChild,
+    ApplicationRef, ChangeDetectorRef,
+    Component,
+    ComponentFactoryResolver,
+    ComponentRef,
+    ElementRef,
+    EmbeddedViewRef,
+    Injector,
+    ViewChild,
     ViewContainerRef
 } from "@angular/core";
 import {ClassExampleComponent} from "../class-example/classExampleComponent";
@@ -47,11 +54,15 @@ export class DynamicComponent {
     public _dynamicComponentClass:any = MDirectiveExampleComponent;
 
     private _componentRef: ComponentRef<any>;
+    private _componentRefViaCreate: ComponentRef<any>;
     private _componentRefInstance: ClassExampleComponent;
 
     constructor(
         private componentFactoryResolver: ComponentFactoryResolver,
-        private view: ViewContainerRef
+        private view: ViewContainerRef,
+        private injector: Injector,
+        private appRef: ApplicationRef,
+        private cdr: ChangeDetectorRef
     ) {}
 
     public ngOnInit() {
@@ -62,19 +73,22 @@ export class DynamicComponent {
         // второй способ
         const componentFactory = this.componentFactoryResolver.resolveComponentFactory(ClassExampleComponent);
 
+        // это тоже что и this._componentRef = componentFactory.create(this.injector);
+        // только createComponent еще и вставляет в вью, а  с create еще нужно сделать
+        // кастомно, смотри пример в ngAfterViewInit
         this._componentRef = this.view.createComponent(componentFactory);
 
         // так получаю инстанс компоненты
         this._componentRefInstance = this._componentRef.instance;
         // так задаю ей свойство
-        this._componentRefInstance.prop = "new prop";
+        // this._componentRefInstance.prop = "new prop";
         // так получаю nativeElement компоненты и ее можно куда-нибудь зааппендить
-        console.log("this._componentRef.location.nativeElement ", this._componentRef.location.nativeElement);
+        // console.log("this._componentRef.location.nativeElement ", this._componentRef.location.nativeElement);
 
-        let dynamicComponentFromSomeDiv = this.viewFromSomeDiv.createComponent(componentFactory);
-
-        console.log("dynamicComponentFromSomeDiv.instance ", dynamicComponentFromSomeDiv.instance); // могу у него менять проперти и делать subscribe на эвент эмиттер
-
+        // let dynamicComponentRefFromSomeDiv = this.viewFromSomeDiv.createComponent(componentFactory);
+        //
+        // console.log("dynamicComponentFromSomeDiv.instance ", dynamicComponentFromSomeDiv.instance); // могу у него менять проперти и делать subscribe на эвент эмиттер
+        //
         const data = {prop: "createComponent prop"};
 
         if (data) {
@@ -88,10 +102,6 @@ export class DynamicComponent {
             this._dynamicComponentClass = ClassExampleComponent;
         }, 3000);
 
-
-        // 4ый способ через директиву host
-        this.host.view.createComponent(componentFactory);
-
     }
 
     ngAfterViewInit() {
@@ -102,6 +112,32 @@ export class DynamicComponent {
         //         this.viewFromSomeDivElementRef.nativeElement.appendChild(node);
         //     }
         // }
+
+
+        /************************************************/
+        // способ показывающий что под капотом делает способ this.view.createComponent(componentFactory);
+        // резолвлю ComponentFactory<T>, создаю componentRef, аттачу его вью во вью приложения,
+        // аппендю к нужному дом элементу, вызываю детект чендж
+        const componentFactory = this.componentFactoryResolver.resolveComponentFactory(ClassExampleComponent);
+        this._componentRefViaCreate = componentFactory.create(this.injector);
+        this.appRef.attachView(this._componentRefViaCreate.hostView);
+
+        const domElement = this._componentRefViaCreate.hostView["rootNodes"][0];
+        document.body.appendChild(domElement);
+
+        // если создаю так, то должен при дестрое сделать, вместо таймаута это надо положить в дестрой
+        // в настоящем кейсе
+        setTimeout(() => {
+            this.appRef.detachView(this._componentRefViaCreate.hostView);
+            this._componentRefViaCreate.destroy();
+        }, 3000);
+        /************************************************/
+        this._componentRefViaCreate.changeDetectorRef.detectChanges();
+
+
+        // 4ый способ через директиву host
+        this.host.view.createComponent(componentFactory);
+        this.cdr.detectChanges();
     }
 
     ngOnDestroy() {
